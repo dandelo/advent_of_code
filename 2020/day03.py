ee6be9925afb598e24d652740c8269766f51969c
Dylan Turner <dylan.turner@bjss.com>
2020-12-04 00:27:39 +0000

    Better day3 solution

--- 2020/day03.py
+++ 2020/day03.py
@@ -1,38 +1,33 @@
 #!/usr/local/bin/python3
 
 import os
+from functools import reduce
 
 def main():
     input_file = get_input_filename()
+    map = [line.rstrip('\n') for line in open(input_file)]
 
-    slope2 = solve(input_file, 3, 1)
-    print(f'Part 1 answer: {slope2}')
+    ans1 = solve(map, [(3, 1)])
+    print(f'Part 1 answer: {ans1}')
 
-    slope1 = solve(input_file, 1, 1)
-    slope3 = solve(input_file, 5, 1)
-    slope4 = solve(input_file, 7, 1)
-    slope5 = solve(input_file, 1, 2)
-    print(f'Part 2 answer: {slope1 * slope2 * slope3 * slope4 * slope5}')
+    ans2 = solve(map, [ (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) ] )
+    print(f'Part 2 answer: {ans2}')
 
-def solve(input_file, right, down):
-    trees_hit = 0
-    with open(input_file) as f:
-        line_no = 0
-        while True:
-            if line_no != 0:
-                for i in range(down - 1): 
-                    f.readline()
-            line = f.readline().strip()
-            if not line:
-                break
-            col = line_no*right % len(line)
-            tree_or_not = line[col]
+def solve(map, routes):
+    total_trees_hit = []
+    map_length = len(map)
+    map_width = len(map[0])
+    for right, down in routes:
+        trees_hit = 0
+        for y in range(0, map_length, down):
+            x = y*right % map_width
+            tree_or_not = map[y][x]
             if tree_or_not == '#':
                 trees_hit += 1
-            line_no += down
-
-    return trees_hit
-                    
+            y += down
+        total_trees_hit.append(trees_hit)
+    
+    return reduce(lambda x, y: x*y, total_trees_hit)
 
 def get_input_filename():
     day = os.path.basename(__file__)[3:5]
