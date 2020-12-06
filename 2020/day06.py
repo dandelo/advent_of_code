32b4edb493cfe39c45eef9313e6e4d6008777e2e
Dylan Turner <dylan.turner@bjss.com>
2020-12-06 21:21:16 +0000

    day6 formatting tweak

--- 2020/day06.py
+++ 2020/day06.py
@@ -3,24 +3,22 @@
 import os
 
 def main():
-    input_file = get_input_filename()
+    with open(get_input_filename()) as f:
+        input = f.read()
 
-    with open(input_file) as f:
-        data = f.read()
-
-    ans1 = solve1(data)
+    ans1 = solve1(input)
     print(f'Part 1 answer: {ans1}')
 
-    ans2 = solve2(data)
+    ans2 = solve2(input)
     print(f'Part 2 answer: {ans2}')
 
 
-def solve1(data):
-    answer_sets = [set(declaration_form.replace('\n', '')) for declaration_form in data.split('\n\n')]
+def solve1(input):
+    answer_sets = [set(declaration_form.replace('\n', '')) for declaration_form in input.split('\n\n')]
     return sum([len(x) for x in answer_sets])
 
-def solve2(data):
-    answer_sets = [declaration_form.split('\n') for declaration_form in data.split('\n\n')]
+def solve2(input):
+    answer_sets = [declaration_form.split('\n') for declaration_form in input.split('\n\n')]
     common_answer_count = 0
     for answer_set in answer_sets:
         common_answer_count += len(set.intersection(*map(set,answer_set)))
